1c9298734892bb9eda84d1d0bf60703f38dec30f
lrbarros <luizricardobarros@gmail.com>
2026-09-01 22:17:08 -0300

    aula2
    Added filtering and plotting of EEG data with highlighted seizure intervals.

--- hands_on/aula1_colab.py
+++ hands_on/aula1_colab.py
@@ -18,3 +18,39 @@ data.shape
 
 print("Número de canais:",data.shape[0])
 print("Número de amostras:",data.shape[1])
+
+raw_filtrado = raw.copy().filter(0.5, 40)
+raw_filtrado.plot(duration = 10, n_channels = 5)
+
+data_filtrado = raw_filtrado.get_data()
+data_filtrado.shape
+
+with open ("chb24-summary.txt", "r") as f:
+    conteudo = f.readlines()
+
+for linha in conteudo[:40]:
+    print(linha.strip())
+
+seizure_intervals_sec = [
+	(480,505),
+	(2451,2476)
+    ]
+sfreq = raw.info["sfreq"]
+
+canal_idx = 0
+signal = data_filtrado[canal_idx]
+
+time = np.arange(len(signal)) / sfreq
+
+plt.figure(figsize=(16,4))
+plt.plot(time, signal, linewidth=0.7)
+
+for i,(start,end) in enumerate(seizure_intervals_sec):
+	plt.axvspan(start, end, color="red", alpha=0,25
+	label="Crise", if i==0 else None)
+
+plt.xlabel("Tempo (s)")
+plt.ylabel("Amplitude")
+plt.title("EEG com intervalos de crise destacados")
+plt.legend()
+plt.show()
